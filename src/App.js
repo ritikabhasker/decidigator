70c5a1486a29e1b2a6e58f1352fa3f4e08a4b570
Ritika Bhasker <ritika.s.bhasker@gmail.com>
2019-11-21 16:00:51 -0500

    styling changes

--- src/App.js
+++ src/App.js
@@ -20,12 +20,26 @@ class App extends Component {
   render() {
     return (
       <div className="App" >
-        <h1>There's a 50% chance of absolutely anything happening on a given day: it either does or it doesn't. Enter a yes or no question and let the universe predict whether or not it's going to happen today!
-           </h1>
+        <h1>
+          There's a 50% chance of absolutely anything happening on a given day: it either does or it doesn't. This is science.
+        </h1>
+        <h2>
+          Enter a yes or no question and let the universe predict whether or not it's going to happen today!
+          </h2>
         <input className='input' placeholder="Type in a yes/no question" onChange={this.handleChange}></input>
-        <button className='button' onClick={this.handleSubmit}>{this.state.toggle ? "Try again you cheat" : "Click me to submit"}</button>
+        <div>
+          <button className='button' onClick={this.handleSubmit}>{this.state.toggle ? "Try again you cheat" : "Click me to submit"}</button>
+        </div>
         {this.state.toggle ?
-          <div className='output'>The answer to {this.state.question} is: {this.state.answer}</div> : null}
+          <div className='output'>
+            The answer to
+            <div className='question output'>{this.state.question}</div> is:
+            <div className='answer'>
+              {this.state.answer}
+            </div>
+          </div>
+          : null
+        }
       </div>
     );
   }
