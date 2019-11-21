b950e4c4f2f18bcf9acd3275ea353d19ed655d57
Ritika Bhasker <ritika.s.bhasker@gmail.com>
2019-11-21 17:21:15 -0500

    more styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     toggle: false
   }
   handleChange = e => {
-    this.setState({ question: e.target.value })
+    this.setState({ question: e.target.value, toggle: false })
   }
   handleSubmit = () => {
     this.setState({ answer: this.generateAnswer(), toggle: !this.state.toggle })
@@ -20,15 +20,18 @@ class App extends Component {
   render() {
     return (
       <div className="App" >
-        <h1>
+        <div className='title'>
+          Decidigator
+        </div>
+        <div className='heading'>
           There's a 50% chance of absolutely anything happening on a given day: it either does or it doesn't. This is science.
-        </h1>
-        <h2>
+        </div>
+        <div className='sub'>
           Enter a yes or no question and let the universe predict whether or not it's going to happen today!
-          </h2>
+        </div>
         <input className='input' placeholder="Type in a yes/no question" onChange={this.handleChange}></input>
         <div>
-          <button className='button' onClick={this.handleSubmit}>{this.state.toggle ? "Try again you cheat" : "Click me to submit"}</button>
+          <button className='button' onClick={this.handleSubmit}>{this.state.toggle ? "Try again, you cheat" : "Click me to submit"}</button>
         </div>
         {this.state.toggle ?
           <div className='output'>
@@ -40,6 +43,8 @@ class App extends Component {
           </div>
           : null
         }
+        {this.state.answer === "Yes" && this.state.toggle ? <img height='400px' src={require('./happy.jpg')} /> : null}
+        {this.state.answer === "No" && this.state.toggle ? <img height='400px' src={require('./sad.jpg')} /> : null}
       </div>
     );
   }
